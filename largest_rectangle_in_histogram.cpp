0350bdc08320027b75db2c09abf67806f4daebc9
david-evan-romero <evanromero2003@icloud.com>
2025-10-29 21:53:16 -0500

    Create largest_rectangle_in_histogram.cpp

---
+++ largest_rectangle_in_histogram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int> st;
+        int maxArea = 0;
+        heights.push_back(0);  // sentinel to flush out remaining bars
+        
+        for (int i = 0; i < heights.size(); i++) {
+            while (!st.empty() && heights[i] < heights[st.top()]) {
+                int h = heights[st.top()];
+                st.pop();
+                int width = st.empty() ? i : i - st.top() - 1;
+                maxArea = max(maxArea, h * width);
+            }
+            st.push(i);
+        }
+        
+        return maxArea;
+    }
+};
